c2b451a91ae56cc8ae84938718994b9064299cf0
Hsi Ho <30316092+mu2er@users.noreply.github.com>
2021-09-16 01:23:34 +0800

    Create iboy1069.cookie.js

---
+++ QX/Scripts/iboy1069.cookie.js
@@ -0,0 +1,68 @@
+const cookieName = 'iBoy1069'
+const signurlKey = 'senku_signurl_iboy1069'
+const signheaderKey = 'senku_signheader_iboy1069'
+const signbodyKey = 'senku_signbody_iboy1069'
+const senku = init()
+
+const requrl = $request.url
+if ($request && $request.method != 'OPTIONS') {
+  const signurlVal = requrl
+  const signheaderVal = JSON.stringify($request.headers)
+  //const signbodyVal = $request.body
+  //const cmd = JSON.parse($request.body).cmd
+  senku.log(`signurlVal:${signurlVal}`)
+  senku.log(`signheaderVal:${signheaderVal}`)
+  //senku.log(`signbodyVal:${signbodyVal}`)
+  if (signurlVal) senku.setdata(signurlVal, signurlKey)
+  if (signheaderVal) senku.setdata(signheaderVal, signheaderKey)
+  senku.msg(cookieName, `获取Cookie: 成功`, ``)
+  //if (signbodyVal && cmd=='task.revisionSignInGetAward') {
+  //  senku.setdata(signbodyVal, signbodyKey)
+  //  senku.msg(cookieName, `获取Cookie: 成功`, ``)
+  //}  
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+senku.done()
